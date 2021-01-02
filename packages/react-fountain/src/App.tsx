64572bb7f545219563ef9c5422e8ed0e5a93c472
ty.h <tyler@fountain.finance>
2021-01-01 23:25:22 -0500

    basic scaffolding
    formatting

--- packages/react-fountain/src/App.tsx
+++ packages/react-fountain/src/App.tsx
@@ -1,26 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import './App.scss'
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import CreateMp from './components/CreateMp'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            some /
+          </Route>
+          <Route exact path="/create">
+            <CreateMp />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ packages/react-fountain/src/components/CreateMp.tsx
@@ -0,0 +1,3 @@
+export default function CreateMp() {
+  return <div>Create MP</div>
+}
