7e7682b20ee7221bb7c610a5da37e44e138d457f
João Ritter <ritterjv@gmail.com>
2021-01-04 22:00:35 -0500

    ~

--- packages/hardhat/contracts/examples/YourContract.sol
+++ packages/hardhat/contracts/examples/YourContract.sol
@@ -6,7 +6,6 @@ import "../abstract/MoneyPoolOwner.sol";
 /// @dev This contract is an example of how you can use Fountain to fund your own project.
 contract YourContract is MoneyPoolOwner {
     constructor(IFountain _fountain, IERC20 _want)
-        public
         MoneyPoolOwner(_fountain, 10000, 60, _want)
     {}
 }
