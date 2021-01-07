80074128fca149ae0ae0ba7d6814149b35538ad8
ty.h <tyler@fountain.finance>
2021-01-06 20:38:07 -0500

    remove unused

--- packages/app/src/components/Navbar.tsx
+++ packages/app/src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import { JsonRpcProvider } from '@ethersproject/providers'
 
 import Account from './Account'
-import Tab from './Tab'
 
 export default function Navbar({
   address,
@@ -12,44 +11,17 @@ export default function Navbar({
   userProvider?: JsonRpcProvider
   onConnectWallet: VoidFunction
 }) {
-  // Key tabs by index
-  const tabs = [
-    Tab({
-      name: 'Create',
-      link: '/create',
-    }),
-  ].map((tab, key) => ({
-    ...tab,
-    key,
-  }))
-
-  // const showFaucet =
-  //   localProvider?.connection?.url?.indexOf('localhost') >= 0 &&
-  //   userProvider?.connection?.url.includes('unknown') &&
-  //   !process.env.REACT_APP_PROVIDER &&
-  //   address
-
   return (
     <div
       style={{
         display: 'flex',
         alignItems: 'baseline',
-        justifyContent: 'space-between',
+        justifyContent: 'flex-end',
         padding: 20,
         borderBottom: '1px solid lightgrey',
       }}
     >
-      <div>{tabs}</div>
-      <div>
-        {/* {showFaucet ? (
-          <span style={{ marginRight: 30 }}>
-            <Faucet address={address} />
-          </span>
-        ) : (
-          ''
-        )} */}
-        <Account userProvider={userProvider} loadWeb3Modal={onConnectWallet} address={address}></Account>
-      </div>
+      <Account userProvider={userProvider} loadWeb3Modal={onConnectWallet} address={address}></Account>
     </div>
   )
 }
