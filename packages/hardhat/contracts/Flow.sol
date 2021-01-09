3e4cd5c4c404d111b19bd9ecb40af65a75733459
João Ritter <ritterjv@gmail.com>
2021-01-09 13:55:02 -0500

    first

--- packages/hardhat/contracts/Flow.sol
+++ packages/hardhat/contracts/Flow.sol
@@ -4,22 +4,7 @@ pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Flow is ERC20 {
-    address public treasury;
-
-    modifier onlyTreasury {
-        require(
-            msg.sender == treasury,
-            "Only the treasury can call this function."
-        );
-        _;
-    }
-
-    constructor(uint256 _initialSupply) public ERC20("Overflow", "FLOW") {
-        treasury = msg.sender;
+    constructor(uint256 _initialSupply) public ERC20("Overflow", "FLOW", 18) {
         _mint(msg.sender, _initialSupply);
-    }
-
-    function mint(uint256 _amount) external onlyTreasury {
-        _mint(treasury, _amount);
     }
 }
